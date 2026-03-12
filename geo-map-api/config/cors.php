37c69b32d274a019f89b762d615bbf78b43a5ac7
DH540 <dylanaguillosalazar@gmail.com>
2026-03-12 14:46:37 +0800

    small edit

--- geo-map-api/config/cors.php
+++ geo-map-api/config/cors.php
@@ -21,7 +21,8 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
-    http_response_code(200);
+    header("Access-Control-Max-Age: 86400");
+    http_response_code(204);
     exit();
 }
 ?>
